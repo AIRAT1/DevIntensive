8824362f20e9bc99b54a69912278ad325dd7da8c
Ayrat <ayrat1@mail.ru>
2016-06-27 22:38:06 +0300

    make all from video 01.10.58

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -2,29 +2,42 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mNavigationDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
         mCallImg = (ImageView)findViewById(R.id.call_img);
-        mCallImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showProgress();
-                runWithDelay();
-            }
-        });
+        mCoordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_Coordinator_container);
+        mNavigationDrawer = (DrawerLayout)findViewById(R.id.navigation_drawer);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setupToolbar();
+        mCallImg.setOnClickListener(this);
+        if (savedInstanceState == null) {
+            showSnackbar("Активити запускается впервые");
+        }else {
+            showSnackbar("Активити уже создавалось");
+        }
     }
 
     @Override
@@ -62,6 +75,23 @@ public class MainActivity extends BaseActivity {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+    private void showSnackbar(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
     private void runWithDelay() {
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -70,5 +100,34 @@ public class MainActivity extends BaseActivity {
                 hideProgress();
             }
         }, 5000);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.call_img:
+                showProgress();
+                runWithDelay();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mNavigationDrawer.isDrawerOpen(GravityCompat.START)) {
+            mNavigationDrawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
