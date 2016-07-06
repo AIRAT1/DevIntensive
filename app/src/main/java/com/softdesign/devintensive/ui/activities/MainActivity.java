57bdcc855711ae363bb74f5470f5840f6cb3cd30
Ayrat <ayrat1@mail.ru>
2016-07-06 09:21:58 +0300

    01.41.00

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,20 +1,27 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -97,6 +104,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         loadUserInfoValue();
         Picasso.with(this)
                 .load(mDataManager.getPreferencesManager().loadUserPhoto())
+                .placeholder(R.drawable.germany) //todo сделать placeholder + transform + crop
                 .into(mProfileImage);
 
 //        List<String> test = mDataManager.getPreferencesManager().loadUserProfileData();
@@ -301,19 +309,50 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
                 ConstantManager.REQUEST_GALLERY_PICTURE);
     }
     private void loadPhotoFromCamera() {
+        if
+                (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
 
-        Intent takeCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            mPhotoFile = createImageFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-            // todo обработать ошибку
-        }
-        if (mPhotoFile != null) {
-            takeCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhotoFile));
-            startActivityForResult(takeCaptureIntent, ConstantManager.REQUEST_CAMERA_PICTURE);
+            Intent takeCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try {
+                mPhotoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                // todo обработать ошибку
+            }
+            if (mPhotoFile != null) {
+                takeCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhotoFile));
+                startActivityForResult(takeCaptureIntent, ConstantManager.REQUEST_CAMERA_PICTURE);
+            }
+        }else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, ConstantManager.CAMERA_REQUEST_PERMISSION_CODE);
+            Snackbar.make(mCoordinatorLayout, "Для корректной работы необходимо дать требуемые разрешения",
+                    Snackbar.LENGTH_LONG).setAction("Разрешить", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openApplicationSettings();
+                }
+            }).show();
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == ConstantManager.CAMERA_REQUEST_PERMISSION_CODE && grantResults.length == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //// TODO: 06.07.2016  
+            }
+            if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                //// TODO: 06.07.2016
+            }
+        }
+    }
+
     private void hideProfilePlaceholder() {
         mProfilePlaceholder.setVisibility(View.GONE);
     }
@@ -345,15 +384,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
                                 switch (choiceItem) {
                                     case 0:
                                         loadPhotoFromGallery();
-                                        showSnackbar("Загрузить из галлереи");
+//                                        showSnackbar("Загрузить из галлереи");
                                         break;
                                     case 1:
                                         loadPhotoFromCamera();
-                                        showSnackbar("Загрузить из камеры");
+//                                        showSnackbar("Загрузить из камеры");
                                         break;
                                     case 2:
                                         dialog.cancel();
-                                        showSnackbar("Отменить");
+//                                        showSnackbar("Отменить");
                                         break;
                                     default:
                                         break;
@@ -371,12 +410,24 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, image.getAbsolutePath());
+
+        this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         return image;
     }
     private void insertProfileImage(Uri selectedImage) {
         Picasso.with(this)
                 .load(selectedImage)
                 .into(mProfileImage);
+        // todo сделать placeholder + transform + crop
         mDataManager.getPreferencesManager().saveUserPhoto(selectedImage);
+    }
+    public void openApplicationSettings() {
+        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(appSettingsIntent, ConstantManager.PERMISSION_REQUEST_SETTINGS_CODE);
     }
 }
