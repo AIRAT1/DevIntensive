7e50ef2bf14add03d3f7c1fdd710d00bfc47c750
Ayrat <ayrat1@mail.ru>
2016-07-07 23:02:39 +0300

    add LoginActivity

---
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.softdesign.devintensive.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.softdesign.devintensive.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = (Button)findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
