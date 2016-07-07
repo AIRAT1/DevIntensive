f96811c5ffcf1ee7b686a80bcd60d2993ed30e7d
Ayrat <ayrat1@mail.ru>
2016-07-08 00:01:18 +0300

    add fab in activity_login

--- app/src/main/java/com/softdesign/devintensive/ui/activities/LoginActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/LoginActivity.java
@@ -2,21 +2,33 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.softdesign.devintensive.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnLogin;
+    private Button mBtnlogin;
+    private FloatingActionButton mFab;
+    private TextInputLayout mInputLayoutLoginEmail, mInputLayoutLoginPassword;
+    private EditText mLoginEmailEt, mLoginPasswordEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnLogin = (Button)findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        mBtnlogin = (Button)findViewById(R.id.btn_login);
+        mFab = (FloatingActionButton)findViewById(R.id.fab);
+        mInputLayoutLoginEmail = (TextInputLayout)findViewById(R.id.input_layout_login_email);
+        mInputLayoutLoginPassword = (TextInputLayout)findViewById(R.id.input_layout_login_password);
+        mLoginEmailEt = (EditText)findViewById(R.id.login_email_et);
+        mLoginPasswordEt = (EditText)findViewById(R.id.login_password_et);
+
+        mBtnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
