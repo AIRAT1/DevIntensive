cb44571a1ee2a73523b63d411f59d6c4f2a66f70
Ayrat <ayrat1@mail.ru>
2016-07-11 23:10:33 +0300

    LoginActivity and activity_login.xml refactoring

--- app/src/main/java/com/softdesign/devintensive/ui/activities/LoginActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/LoginActivity.java
@@ -2,21 +2,18 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.softdesign.devintensive.R;
 
 public class LoginActivity extends AppCompatActivity {
     private Button mBtnlogin;
-    private FloatingActionButton mFab;
-    private TextInputLayout mInputLayoutLoginEmail, mInputLayoutLoginPassword;
-    private EditText mLoginEmailEt, mLoginPasswordEt;
+    private TextInputLayout mWrapLoginEmail, mWrapLoginPassword;
+    private EditText mEtLoginEmail, mEtLoginPassword;
 
     /**
      * bind values
@@ -26,12 +23,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mBtnlogin = (Button)findViewById(R.id.btn_login);
-        mFab = (FloatingActionButton)findViewById(R.id.fab);
-        mInputLayoutLoginEmail = (TextInputLayout)findViewById(R.id.input_layout_login_email);
-        mInputLayoutLoginPassword = (TextInputLayout)findViewById(R.id.input_layout_login_password);
-        mLoginEmailEt = (EditText)findViewById(R.id.login_email_et);
-        mLoginPasswordEt = (EditText)findViewById(R.id.login_password_et);
+        mBtnlogin = (Button)findViewById(R.id.login_btn);
+        mWrapLoginEmail = (TextInputLayout)findViewById(R.id.wrap_login_email);
+        mWrapLoginPassword = (TextInputLayout)findViewById(R.id.wrap_login_password);
+        mEtLoginEmail = (EditText)findViewById(R.id.et_login_email);
+        mEtLoginPassword = (EditText)findViewById(R.id.et_login_password);
 
         mBtnlogin.setOnClickListener(new View.OnClickListener() {
             /**
@@ -43,13 +39,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
-    }
-
-    /**
-     * make toast when fab is clicked
-     * @param view
-     */
-    public void fabOnClick(View view) {
-        Toast.makeText(LoginActivity.this, "fab onClick", Toast.LENGTH_SHORT).show();
     }
 }
